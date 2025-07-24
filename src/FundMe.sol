8a6a35dcc5c1e9c301434de50da673b0fdc3dfe2
Alucccard <ai454293996@gmail.com>
2025-07-24 17:52:23 +0800

    to lesson 21

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -15,7 +15,7 @@ contract FundMe {
     address[] private s_funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address private immutable i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
     AggregatorV3Interface private s_priceFeed;
 
@@ -39,6 +39,19 @@ contract FundMe {
         // require(msg.sender == owner);
         if (msg.sender != i_owner) revert NotOwner();
         _;
+    }
+
+    function cheaperWithdraw() public onlyOwner {
+        //so that we can save some gas from avoid the array length check everytime
+        uint256 fundersCount = s_funders.length;
+        for (uint256 funderIndex = 0; funderIndex < fundersCount; funderIndex++) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
     function withdraw() public onlyOwner {
@@ -86,6 +99,10 @@ contract FundMe {
 
     function getFunder(uint256 index) public view returns (address) {
         return s_funders[index];
+    }
+
+    function getOwner() external view returns (address) {
+        return i_owner;
     }
 }
 
--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -12,6 +12,12 @@ contract FundMeTest is Test {
 
     uint256 constant SEND_VALUE = 10000000000000000; // 0.01 ETH in wei
 
+    modifier funded() {
+        vm.prank(USER); // Simulate a transaction from USER
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
     function setUp() public {
         // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         FundMeScript fundMeScript = new FundMeScript();
@@ -28,7 +34,7 @@ contract FundMeTest is Test {
 
     function testContractOwner() public view {
         // Check if the contract owner is set correctly
-        assertEq(fundMe.i_owner(), msg.sender, "Contract owner should be the deployer");
+        assertEq(fundMe.getOwner(), msg.sender, "Contract owner should be the deployer");
     }
 
     function testPriceFeedVersionIsAccurate() public view {
@@ -42,10 +48,100 @@ contract FundMeTest is Test {
         fundMe.fund{value: 0}(); // This will fail because no ETH is sent
     }
 
-    function testFundUpdatesFundedDataStructure() public {
-        vm.prank(USER); // Simulate a transaction from USER
-        fundMe.fund{value: SEND_VALUE}();
+    function testFundUpdatesFundedDataStructure() public funded {
         uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
         assertEq(amountFunded, SEND_VALUE, "Amount funded should be 0.01 ETH in wei");
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER, "Funder should be USER's address");
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert("NotOwner()"); // Expect revert with NotOwner error
+        fundMe.withdraw(); // USER tries to withdraw, should fail
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        //arrange
+        uint256 startOwnerBalance = fundMe.getOwner().balance;
+        uint256 startFundMeBalance = address(fundMe).balance;
+        //action
+        vm.prank(fundMe.getOwner()); // Simulate a transaction from the owner
+        fundMe.withdraw();
+        //assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0, "FundMe balance should be zero after withdrawal");
+        assertEq(
+            endingOwnerBalance,
+            startOwnerBalance + startFundMeBalance,
+            "Owner balance should increase by the FundMe balance after withdrawal"
+        );
+    }
+
+    function testWithdrawWithMultipleFunders() public funded {
+        //arange
+        //build X number of funders
+        uint160 numberOfFunders = 10;
+        //a index to track the funders
+        uint160 funderIndex = 1;
+
+        //use a for loop to build the funders and fund the contract
+        for (uint160 i = funderIndex; i < numberOfFunders; i++) {
+            //hoax creates a new address and sends SEND_VALUE to it
+            hoax(address(i), SEND_VALUE); // Simulate a transaction from each funder with SEND_VALUE
+            //contract is funded by each funder
+            fundMe.fund{value: SEND_VALUE}(); // Each funder funds the contract
+        }
+
+        //act
+        uint256 startOwnerBalance = fundMe.getOwner().balance;
+        uint256 startFundMeBalance = address(fundMe).balance;
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw(); // Owner withdraws funds
+        vm.stopPrank();
+        //assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0, "FundMe balance should be zero after withdrawal");
+        assertEq(
+            startOwnerBalance + startFundMeBalance,
+            endingOwnerBalance,
+            "Owner balance should increase by the FundMe balance after withdrawal"
+        );
+    }
+
+    function testWithdrawWithMultipleFundersCheaper() public funded {
+        //arange
+        //build X number of funders
+        uint160 numberOfFunders = 10;
+        //a index to track the funders
+        uint160 funderIndex = 1;
+
+        //use a for loop to build the funders and fund the contract
+        for (uint160 i = funderIndex; i < numberOfFunders; i++) {
+            //hoax creates a new address and sends SEND_VALUE to it
+            hoax(address(i), SEND_VALUE); // Simulate a transaction from each funder with SEND_VALUE
+            //contract is funded by each funder
+            fundMe.fund{value: SEND_VALUE}(); // Each funder funds the contract
+        }
+
+        //act
+        uint256 startOwnerBalance = fundMe.getOwner().balance;
+        uint256 startFundMeBalance = address(fundMe).balance;
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw(); // Owner withdraws funds
+        vm.stopPrank();
+        //assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0, "FundMe balance should be zero after withdrawal");
+        assertEq(
+            startOwnerBalance + startFundMeBalance,
+            endingOwnerBalance,
+            "Owner balance should increase by the FundMe balance after withdrawal"
+        );
     }
 }
