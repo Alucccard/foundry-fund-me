89bc3034198e28c163194e1964e102e2553dcc78
Gloomwing <ai454293996@outlook.com>
2025-07-25 11:14:05 +0800

    Add unit tests for FundMe contract functionality

--- test/unit/FundMe.t.sol
+++ test/unit/FundMe.t.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol";
-import {FundMeScript} from "../script/FundMe.s.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {FundMeScript} from "../../script/FundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
